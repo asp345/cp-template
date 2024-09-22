3fb42a53f1eabf2008d07816f61e23a9d7fce72f
Chanyeong Lim <dlacksdud2@gmail.com>
2024-09-22 22:27:13 +0900

    update cpp

--- cpp_ps.cpp
+++ cpp_ps.cpp
@@ -3,19 +3,31 @@
 #pragma GCC target("arch=haswell")
 using namespace std;
 using i64 = int64_t;
+using u64 = uint64_t;
+using f64 = double;
 using pii = pair<i64, i64>;
 using vi = vector<i64>;
 using v2i = vector<vi>;
+
+const i64 MOD = 1e9 + 7;
+const f64 PI = 3.1415926535897932384626433832795;
+
+#define f(i, s, e) for (i64 i = s; i < e; i++)
+
+i64 gcd(i64 a, i64 b) {
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
 
 int main() {
   i64 n, m, k, res;
 
   cin >> n >> m;
   vi a(n, 0), b(n, 0), c(n, 0);
+  v2i d(n, vi(m, 0));
 
-  for (i64 i = 0; i < n; i++) {
-    cin >> a[i] >> b[i] >> c[i];
-  }
+  f(i, 0, n) { cin >> a[i] >> b[i] >> c[i]; }
 
   res = a[0];
   cout << res;
