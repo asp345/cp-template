96b2b7e0ce6cfd3ac70f9400437ed2f9578b6516
Chanyeong Lim <dlacksdud2@gmail.com>
2024-10-04 22:29:00 +0900

    update

--- cpp_ps.cpp
+++ cpp_ps.cpp
@@ -2,23 +2,18 @@
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("arch=haswell")
 using namespace std;
-using i64 = int64_t;
-using u64 = uint64_t;
-using f64 = double;
-using pii = pair<i64, i64>;
+using i64 = long long;
+using u64 = unsigned long long;
+using f64 = long double;
+using pi64 = pair<i64, i64>;
 using vi = vector<i64>;
 using v2i = vector<vi>;
 
 const i64 MOD = 1e9 + 7;
 const f64 PI = 3.1415926535897932384626433832795;
+const i64 INF = (~0U >> 2);
 
 #define f(i, s, e) for (i64 i = s; i < e; i++)
-
-i64 gcd(i64 a, i64 b) {
-  if (b == 0)
-    return a;
-  return gcd(b, a % b);
-}
 
 int main() {
   i64 n, m, k, res;
@@ -29,7 +24,7 @@ int main() {
 
   f(i, 0, n) { cin >> a[i] >> b[i] >> c[i]; }
 
-  res = a[0];
+  res = 0;
   cout << res;
   return 0;
 }
