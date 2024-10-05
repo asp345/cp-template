8f96f201a304d2ee82816b76f809da00dad8e9e0
Chanyeong Lim <dlacksdud2@gmail.com>
2024-10-05 20:35:19 +0900

    update

--- cpp_ps.cpp
+++ cpp_ps.cpp
@@ -2,29 +2,42 @@
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("arch=haswell")
 using namespace std;
-using i64 = long long;
-using u64 = unsigned long long;
-using f64 = long double;
+using i64 = int64_t;
+using u64 = uint64_t;
+using f64 = double;
+using f80 = long double;
 using pii = pair<i64, i64>;
-using vi = vector<i64>;
-using v2i = vector<vi>;
+template <class T>
+using Vec = vector<T>;
+template <class K, class V>
+using UM = unordered_map<K, V>;
+using vi = Vec<i64>;
 
 const i64 MOD = 1e9 + 7;
-const f64 PI = 3.1415926535897932384626433832795;
+const f80 PI = 3.1415926535897932384626433832795;
 const i64 INF = (~0U >> 2);
 
 #define f(i, s, e) for (i64 i = s; i < e; i++)
+#define all(x) (x).begin(), (x).end()
+
+i64 t, n, m, k, res;
 
 int main() {
-  i64 n, m, k, res;
+  // fastio
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> t;
+  f(_, 0, t) {
+    // testcases
+  }
 
   cin >> n >> m;
-  vi a(n, 0), b(n, 0), c(n, 0);
-  v2i d(n, vi(m, 0));
+  vi a(n, 0);
+  f(i, 0, n) {
+    cin >> a[i];
+  }
 
-  f(i, 0, n) { cin >> a[i] >> b[i] >> c[i]; }
-
-  res = 0;
   cout << res;
   return 0;
 }
--- py_ps.py
+++ py_ps.py
@@ -1,6 +1,8 @@
 import sys
-sys.setrecursionlimit(10000)
-input = sys.stdin.readline
+sys.setrecursionlimit(100002)
+sys.set_int_max_str_digits(100000)
+inp = lambda: sys.stdin.readline().rstrip()
 
-n, m = map(int, input().split())
-print(n)
+for _ in range(int(inp())):
+    n, m = map(int, input().split())
+    print(n)
